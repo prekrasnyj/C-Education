2b32d513a257248b6a36e9cdf16e586796f26d4f
Prekrasnyj <horutog@mail.com>
2022-08-24 23:25:33 +0900

    Добавили пример с методами по работе с массивом

---
+++ Example010_array/Program.cs
@@ -0,0 +1,51 @@
+﻿//Метод заполняющий готовый массив рандомными числами от 1 до 10
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+//Метод, выводящий все эелменты массива в консоль
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int [] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[2] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 44);
+Console.WriteLine(pos);
