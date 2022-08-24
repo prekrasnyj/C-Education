ddadd90c96ae18171636e2a9c084a6974831d753
Prekrasnyj <horutog@mail.com>
2022-08-24 23:01:08 +0900

    Добавили пример с поиском нужного числа в массиве

---
+++ Example009_method/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 14, 22, 64, 55, 26, 55, 38 };
+
+int n =  array.Length;
+int find = 55;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
