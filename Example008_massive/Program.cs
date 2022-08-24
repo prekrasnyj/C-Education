c5ff17c5a8eb27e7da629b875870ce23dd75b07c
Prekrasnyj <horutog@mail.com>
2022-08-24 22:51:21 +0900

    Добавили пирмер с массивом и функцией

---
+++ Example008_massive/Program.cs
@@ -0,0 +1,19 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//              0   1   2   3   4   5   6   7   8
+int[] array = { 12, 25, 32, 45, 35, 26, 67, 78, 39 };
+array[0] = 11;
+Console.WriteLine(array[0]);
+
+
+int max =  Max(
+    Max(array[0], array[1],array[2]),
+    Max(array[3], array[4],array[5]),
+    Max(array[6], array[7],array[8])
+);
+Console.WriteLine(max);
