83372fea14e15b1a54b7e99574f40f2d49480fe9
Prekrasnyj <horutog@mail.com>
2022-08-30 16:42:42 +0900

    add for

--- Example011_methods/Program.cs
+++ Example011_methods/Program.cs
@@ -1,49 +1,72 @@
-﻿//вид 1
-void Method1()
-{
-    Console.WriteLine("Автор  ...");
-}
-Method1();
+﻿// //вид 1
+// void Method1()
+// {
+//     Console.WriteLine("Автор  ...");
+// }
+// Method1();
 
 
-//Method 2
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
-Method2("Текст сообщения");
+// //Method 2
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2("Текст сообщения");
 
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i<count)
-    {
-        Console.WriteLine(msg);
-        i++;
-    }
-}
-Method21(count: 4, msg: "Текст сообщения 2");
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i<count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(count: 4, msg: "Текст сообщения 2");
 
-//3 вид
-int Method3()
-{
-    return DateTime.Now.Year;
-}
-int year = Method3();
-Console.WriteLine(year);
+// //3 вид
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// Console.WriteLine(year);
 
-//4 вид
+// //4 вид
+// string Method4(int count, string c)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i<count)
+//     {
+//         result = result + c;
+//         i++;
+//     }
+//     return result;
+// }
+
+//string res = Method4(10, "фывфы");
+//Console.WriteLine(res);
+
 string Method4(int count, string c)
 {
-    int i = 0;
+
     string result = String.Empty;
-    while (i<count)
+
+    for (int i = 0; i < count; i++)
     {
         result = result + c;
-        i++;
     }
     return result;
 }
-
 string res = Method4(10, "фывфы");
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 0; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
