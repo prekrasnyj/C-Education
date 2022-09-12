d8e8085068ac68930841f13909799cf2c761071b
Prekrasnyj <horutog@mail.com>
2022-09-12 14:16:29 +0900

    add homework 6

---
+++ Homework6/Task41/Program.cs
@@ -0,0 +1,50 @@
+﻿//Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+//Метод создает массив из М чисел и заполняется вручную
+int [] CreateManualArray(int m)
+{
+    int [] array = new int [m];
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");  
+        array[i] = Convert.ToInt32(Console.ReadLine());   
+    }
+    return array;
+}
+
+//метод вывода массива в консоль
+void ShowMassive(int[] massive)
+{
+    int n = massive.Length;
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write(massive[i]);
+        if (i < n-1) Console.Write(", ");
+    }
+    Console.WriteLine();
+}
+
+//Метод считает, сколько чисел больше 0 введет пользователь
+int CountPositiveNumbers(int [] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) {sum++;}
+
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Сколько элементов массива вы хотите ввести?");
+int M = Convert.ToInt32(Console.ReadLine());
+
+//Выводим рандомный массив
+int[] newMassive = CreateManualArray(M);
+ShowMassive(newMassive);
+int result = CountPositiveNumbers(newMassive);
+Console.Write("Колличество чисел > 0 - ");
+Console.WriteLine(result);
---
+++ Homework6/Task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+//Метод проверяет пересекаются ли прямые
+void SearchIntersectionPoints(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2 & b1 == b2) { Console.WriteLine("Прямые совпадают"); }
+    else if (k1 == k2) { Console.WriteLine("Прямые параллельны"); }
+    else
+    {
+        double x = -(b1 - b2) / (k1 - k2);
+        double y = k1 *x + b1;
+        x = Math.Round(x, 1);
+        y = Math.Round(y, 1);
+        Console.WriteLine($"Прямые пересекаются в точке: ({x};{y})"); 
+    }
+
+}
+
+
+Console.Write("Введите b1 - ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1 - ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2 - ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2 - ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+SearchIntersectionPoints(b1, k1, b2, k2);
