870776846ca9144ffac9a75d11964bfa3769c8e2
Prekrasnyj <horutog@mail.com>
2022-09-17 17:57:17 +0900

    add

---
+++ Example015_classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+enum Color
+{
+    Red,
+    Green,
+    Orange
+}
+class Point
+{
+    public int x;
+    public int y;
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point p = new Point();
+        p.x = 4;
+        p.y = 2;
+
+        Point p2 = new Point();
+        p2.x = 2;
+        p2.y = 6;
+
+        Console.WriteLine(p.x);
+        Console.WriteLine(p.y);
+        Console.WriteLine($"X: {p.x}  |  Y: {p.y}");
+    }
+}
